6d27895de231a0e8324f0654c7a7a42fecb31394
Gayathri <103631083+gr2100@users.noreply.github.com>
2023-11-04 15:05:36 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# Welcome to my Data Science Notebook. In this notebook, I will explore various data science concepts and tools.
+# 
+
+# Data Science Languages
+# 1. Python
+# 2. R
+# 3. SQL
+# 
+
+# Data Science Libraries
+# 
+# 1. Pandas
+# 2. NumPy
+# 3. Matplotlib
+# 4. Seaborn
+# 5. Scikit-Learn
+# 
+
+# ## Data Science Tools
+# 
+# | Tool           | Description               |
+# |----------------|---------------------------|
+# | Jupyter Notebook| Interactive computing   |
+# | SQL Server     | Relational database      |
+# | Tableau        | Data visualization        |
+# | TensorFlow     | Machine learning framework|
+# | Keras          | Deep learning framework  |
+# 
+
+# 
+# ### Let's explore some arithmetic expressions using Python with some examples below :
+
+# In[16]:
+
+
+# Multiplication and Addition
+a = 5
+b = 3
+result_multiply = a * b
+result_addition = a + b
+result_multiply, result_addition
+
+
+# In[15]:
+
+
+# Convert Minutes to Hours
+minutes = 120
+hours = minutes / 60
+hours
+
+
+# __Objectives__
+# In this notebook, we aim to:
+# 1. Explore data science languages and libraries.
+# 2. Create and evaluate arithmetic expressions.
+# 3. Share our work using GitHub.
+# 
+
+# ## Author
+# This notebook was created by Gayathri R.
+# 
+
+# In[ ]:
+
+
+
+
